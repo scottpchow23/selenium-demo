b592bfc0bd9cc319945bfab4985f49a7480ce7b6
Scott Chow <tehchowster@gmail.com>
2019-11-04 15:11:00 -0800

    sc - works when spring boot application is running in the background

--- src/test/java/com/ucsb56/seleniumdemo/ChromeTest.java
+++ src/test/java/com/ucsb56/seleniumdemo/ChromeTest.java
@@ -4,11 +4,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
 public class ChromeTest {
 
   private WebDriver driver;
@@ -31,11 +38,20 @@ public class ChromeTest {
   }
 
   @Test
-  public void test() {
+  public void googleTest() {
     // Your test code here
     driver.get("https://www.google.com");
     String title = driver.getTitle();
 
     assert (title.equals("Google"));
+  }
+
+  @Test
+  public void indexCheck() {
+    driver.get("http://localhost:8080");
+    WebElement working = driver.findElement(By.id("working"));
+
+    assert (working.isDisplayed());
+    assert (working.getText().equals("It works!"));
   }
 }
